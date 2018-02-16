1f81705c2f43b7b667369960bb16b749df1569c8
Rob Paskin <rjpaskin+git@gmail.com>
2018-02-16 09:25:42 +0000

    Add caching middleware to Octokit

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 
 gem "dotenv", "~> 2.2", require: "dotenv/load"
+gem "faraday-http-cache", "~> 2.0"
 gem "haml", "~> 5.0"
 gem "octokit", "~> 4.8"
 gem "sinatra", "~> 2.0"
--- setup.rb
+++ setup.rb
@@ -1,3 +1,9 @@
 Octokit.configure do |c|
   c.access_token = ENV["GITHUB_ACCESS_TOKEN"]
 end
+
+Octokit.middleware = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::HttpCache, serializer: Marshal, shared_cache: false
+  builder.use Octokit::Response::RaiseError
+  builder.adapter Faraday.default_adapter
+end
